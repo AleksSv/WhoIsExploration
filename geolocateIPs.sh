69b90c357db34831ce2512916984f86c82dd062c
Aleks S <sverdlovsa@gmail.com>
2014-11-20 23:45:46 -0600

    deleted extra files

--- geolocateIPs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILE="ipget.csv"
-while read count ip; do
-        url=http://ip-api.com/csv/$ip
-        result=$(curl --request GET $url)
-        echo "$ip,$count,$url,$result"
-done >$FILE
